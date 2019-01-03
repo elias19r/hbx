1a204c49c62b634331ee33682fb5a348419d185f
Elias Rodrigues <elias.rodrigues@usp.br>
2019-01-03 19:53:39 -0200

    Add asset validate

--- asset/assetvalidate.go
+++ asset/assetvalidate.go
@@ -1,10 +1,21 @@
 package asset
 
+import "log"
+
 // Minimum and maximum scale a valid Asset must have.
 const (
 	MinScale = 0
 	MaxScale = 18
 )
+
+func (a *Asset) Validate() error {
+	if a == nil {
+		err := ErrNil.WithMessage("Validate(): a is nil")
+		log.Println(err)
+		return err
+	}
+	return validateScale(a.scale)
+}
 
 func validateScale(scale int) error {
 	if scale < MinScale || scale > MaxScale {
